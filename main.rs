e24c2560c83928216d1a7f168e23072b96c4f1fd
point-o <155404293+point-o@users.noreply.github.com>
2024-09-02 21:48:29 -0600

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::time::{Instant};
+
+fn main() {
+    println!("Enter something:");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("fuck");
+
+    let something = input.trim(); 
+    let mut copy: String = ' '.to_string();
+    let mut int_char: u8 = 19; //start around ascii (space)
+    let mut our_char: char = int_char as char;
+    let start = Instant::now(); //timing our project should be linear-ish O(n)
+
+    for i in 0..something.len() {
+        while our_char != something.chars().nth(i).unwrap(){
+            copy.push(our_char);
+            println!("{}",copy);
+            copy.pop();
+            int_char += 1;
+            our_char = int_char as char;
+        }
+        int_char = 19;
+        copy.push(our_char);
+        println!("{}",copy);
+    }
+
+    for _ in 0..3 {
+        println!();
+    }
+    let duration = start.elapsed();
+    println!("Your message has been ASCII brute forced in {:?} (fuck you)", duration);
+}
